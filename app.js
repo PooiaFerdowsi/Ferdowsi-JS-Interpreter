a59e32ce4023bc2a21db0c855d2a037d805d5194
Paptoos Inc <72940447+Paptoos@users.noreply.github.com>
2020-11-14 11:40:56 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+function run(){
+    text = document.getElementById('code');
+    lines = text.value.split('\n')
+    for (i = 0; i < ab.length; i++) {
+        line = lines[i]
+        words = line.split(' ')
+        if (words[0] == 'چاپ' && words[1] == 'کن'){
+            alert(words[2])
+        }
+      } 
+}
