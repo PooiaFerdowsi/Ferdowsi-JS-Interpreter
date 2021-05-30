53f99140ae02eeac749ac16111e45d77328ef0a4
PooiaFerdowsi <pooia.ferdowsi.is.developer@gmail.com>
2021-05-30 10:10:06 +0430

    issue 4
    see #4

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function run(){
     text = document.getElementById('code');
     lines = text.value.split('\n')
-    for (i = 0; i < ab.length; i++) {
+    for (i = 0; i < lines.length; i++) {
         line = lines[i]
         words = line.split(' ')
         if (words[0] == 'چاپ' && words[1] == 'کن'){
